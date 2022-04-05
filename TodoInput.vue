e9b23a2979d7d2a512719c2278bdcacf5c72d130
YongTaeIn <97088201+YongTaeIn@users.noreply.github.com>
2022-04-05 14:29:55 +0900

    Vue.js_todo LIST

---
+++ TodoInput.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+   
+    <input type="text" v-model="newTodoItem" v-on:keyup.enter="addTodo">
+    <button v-on:click="addTodo">추가</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data(){
+    return{
+      newTodoItem:''
+    }
+  },
+  methods: {
+    addTodo: function() {
+      console.log(this.newTodoItem);
+      //저장하는 로직
+      localStorage.setItem(this.newTodoItem, this.newTodoItem);
+      this.clearInput();
+    },
+    clearInput: function() {
+      this.newTodoItem=""; //비워주기(초기화)
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
